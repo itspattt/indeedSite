8e827148b2d4c6c3bd7e1e218b43a9f19b7410f0
Cortez, Patricio Damian <pcortez3@gatech.edu>
2025-09-23 13:33:19 -0400

    i think main structure is done

--- indeedSite/jobs/urls.py
+++ indeedSite/jobs/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='jobs.index'),
+    path('<int:id>/', views.show, name='jobs.show'),
 ]
--- indeedSite/jobs/views.py
+++ indeedSite/jobs/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Job
+
 
 # Create your views here.
 def index(request):
     return render(request, 'jobs/index.html', {'template_data': {}})
+
+def show(request, id):
+    job = Job.objects.get(id=id)
+    template_data = {}
+    template_data['title'] = job.name
+    return render(request, 'jobs/show.html', {'template_data': template_data})
