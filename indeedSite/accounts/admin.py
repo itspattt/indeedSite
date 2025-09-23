178bbd9a91096139f0671cd20c8ddd9ce5567f34
Cortez, Patricio Damian <pcortez3@gatech.edu>
2025-09-23 12:45:00 -0400

    some links and profile model

--- indeedSite/accounts/admin.py
+++ indeedSite/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'headline')
+    search_fields = ('user__username', 'headline', 'skills')
+admin.site.register(Profile, ProfileAdmin)
--- indeedSite/accounts/models.py
+++ indeedSite/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255, blank=True)
+    skills = models.TextField(blank=True, help_text="Comma-separated list of skills")
+    education = models.TextField(blank=True)
+    experience = models.TextField(blank=True)
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
